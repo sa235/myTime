31b6fc0632e85abc0bfee789b9b2535f4ab6b03c
sa235 <sa235@live.ru>
2018-08-10 01:01:49 +0500

    Second lern stap

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 from flask import Flask
+from flask import render_template
+from datetime import datetime
+import re
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return 'Hello, Flask'
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d, %B, %Y at %X")
+
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It`s " + formatted_now
+    return content
+
+@app.route("/hello2/<name>")
+def hello_there2(name):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
