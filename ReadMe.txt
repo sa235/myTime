68a9bef4d8e752d88da1d5d27496365b8225b167
sa235 <sa235@live.ru>
2018-08-10 09:41:26 +0500

    add requirements pip freeze

---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+py -3 -m venv env
+pip install flask
+
+Optional:
+pip freeze > requirements.txt
+pip install -r requirements.txt
--- app.py
+++ app.py
@@ -10,24 +10,9 @@ def home():
     return 'Hello, Flask'
 
 @app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d, %B, %Y at %X")
-
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It`s " + formatted_now
-    return content
-
-@app.route("/hello2/<name>")
 def hello_there2(name):
     return render_template(
         "hello_there.html",
-        name=name,
+        name=name.capitalize(),
         date=datetime.now()
     )
